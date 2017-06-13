d507bb71685e48ff32059ee23c4cd9afebd3b024
AlvarDev <deivid07.21@gmail.com>
2017-06-13 14:21:58 -0300

    send notifications

--- firebaseController.js
+++ firebaseController.js
@@ -3,27 +3,27 @@ var data;
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyCIzjIx6J1hTAlg0AEZT4WKxQhmdlnq-L4",
-    authDomain: "santos-digital-demos.firebaseapp.com",
-    databaseURL: "https://santos-digital-demos.firebaseio.com",
-    projectId: "santos-digital-demos",
-    storageBucket: "santos-digital-demos.appspot.com",
-    messagingSenderId: "387503652832"
-  };
+  apiKey: "AIzaSyCIzjIx6J1hTAlg0AEZT4WKxQhmdlnq-L4",
+  authDomain: "santos-digital-demos.firebaseapp.com",
+  databaseURL: "https://santos-digital-demos.firebaseio.com",
+  projectId: "santos-digital-demos",
+  storageBucket: "santos-digital-demos.appspot.com",
+  messagingSenderId: "387503652832"
+};
 
 firebase.initializeApp(config);
 
 dbRef = firebase.database().ref().child("notifications");
-dbRef.on('value', snap => setMessages(snap.val()));
+//dbRef.on('value', snap => setMessages(snap.val()));
 
-function send(){
+function send() {
   var now = new Date();
   var id = now.getTime();
   var urlAvatar = 'https://pbs.twimg.com/profile_images/831958905987010560/0pKEhzWj_400x400.jpg';
   var mTitle = $('#typed-title').val();;
   var mMessage = $('#typed-message').val();
 
-  if(mTitle.length > 0 && mMessage.length > 0){
+  if (mTitle.length > 0 && mMessage.length > 0) {
     var notification = {
       idea: id,
       urlAvatar: urlAvatar,
@@ -32,27 +32,40 @@ function send(){
     };
 
     dbRef.child(id).set(notification);
-    sendNotification();
-  }else{
+    sendNotification(mTitle, mMessage);
+  } else {
     alert("Write a title and a message");
   }
 }
 
-function sendNotification() {
-  
-}
+function sendNotification(mTitle, mMessage) {
+  var not = {
+    notification: {
+      title: mTitle,
+      body: mMessage
+    },
+    to: "/topics/news"
+  };
 
-function setMessages(messages) {
-    /*data = messages;
-    var mes = messages.split("\n");
-    $("#messages").text("");
-    $(".remove").remove();
+  /*****/
+  $.ajax({
+    url: 'https://fcm.googleapis.com/fcm/send',
+    type: 'POST',
+    data: JSON.stringify(not),
+    dataType: "json",
+    beforeSend: function(request) {
+      request.setRequestHeader("Content-Type", "application/json");
+      request.setRequestHeader("Authorization", "key=AAAAWjkEf-A:APA91bGoIMgKi2FZAT1G19YJOaxdY5UsRuK0bqK99Np0shPaY-TGPGlePTbz9A8jUycGSTY-hxu4xwYUiMKbfDIIs9LOu8TJ0d2pGKz48E-WCrBd7le8PC03up0bKarSQBdRfA3P1c7L");
+    },
+    success: function(response) {
+      alert("Message Sent");
+      console.log(response);
+    },
+    error: function(error) {
+      alert("Can't send the message");
+      console.log(error);
+    }
+  });
 
-    var html = "";
 
-    $.each(mes, function(i, item) {
-        html += '<div class="remove">' + item + '</div>';
-    });
-
-    $("#messages").append(html);*/
 }
